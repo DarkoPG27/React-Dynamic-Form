c376453b1d882b589e75b22db309004a90957cad
Darko <89515851+DarkoPG27@users.noreply.github.com>
2022-04-29 13:06:06 +0200

    added formElement

--- src/App.js
+++ src/App.js
@@ -2,9 +2,35 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div classname="App container">
       <h1>React Dynamic Form</h1>
+      <form>
+        <div className="mb-3">
+          <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
+          <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+          <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+        </div>
+        <div className="mb-3">
+          <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
+          <input type="password" className="form-control" id="exampleInputPassword1" />
+        </div>
+        <div className="mb-3 form-check">
+          <input type="checkbox" className="form-check-input" id="exampleCheck1" />
+          <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
+        </div>
+        <select className="form-select" aria-label="Default select example">
+          <option selected>Open this select menu</option>
+          <option value={1}>One</option>
+          <option value={2}>Two</option>
+          <option value={3}>Three</option>
+        </select>
+
+
+
+        <button type="submit" className="btn btn-primary">Submit</button>
+      </form>
     </div>
+
   );
 }
 
